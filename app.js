3f43f4b9f6500f28e1bb374d614973d224e5746b
muhammadhammad002 <mhammadhhs@gmail.com>
2021-07-31 17:46:52 +0500

    quizapp

---
+++ app.js
@@ -0,0 +1,217 @@
+function check () {
+    var score = 0
+    var right_answer_1 = document.getElementById("q1-a1")
+    var q1_answer_2 = document.getElementById("q1-a2")
+    var q1_answer_3 = document.getElementById("q1-a3")
+    var q1_answer_4 = document.getElementById("q1-a4")
+    if (right_answer_1.checked == true){
+        score++
+        alert('Q1 answer right')
+    } 
+    else{
+        alert('Q1 answer wrong')
+    }
+
+
+    var right_answer_3 = document.getElementById("q2-a3")
+    var q2_answer_1 = document.getElementById("q2-a1")
+    var q2_answer_2 = document.getElementById("q2-a2")
+    var q2_answer_4 = document.getElementById("q2-a4")
+    if (right_answer_3.checked == true){
+    score++
+    alert('Q2 answer correct')
+    }
+    else{
+    alert('Q2 answer wrong')
+    }
+
+
+var right_answer_4 = document.getElementById("q3-a4")
+var q3_answer_1 = document.getElementById("q3-a1")
+var q3_answer_2 = document.getElementById("q3-a2")
+var q3_answer_3 = document.getElementById("q3-a3")
+if (right_answer_4.checked == true) {
+    score++
+    alert('Q3 answer correct')
+}
+else{
+    alert('Q3 answer wrong')
+}
+
+    var right_answer_4 = document.getElementById("q4-a4")
+var q4_answer_1 = document.getElementById("q4-a1")
+var q4_answer_2 = document.getElementById("q4-a2")
+var q4_answer_3 = document.getElementById("q4-a3")
+if (right_answer_4.checked == true) {
+    score++
+    alert('Q4 answer correct')
+}
+else{
+    alert('Q4 answer wrong')
+}
+
+
+var right_answer_2 = document.getElementById("q5-a2")
+var q5_answer_1 = document.getElementById("q5-a1")
+var q5_answer_3 = document.getElementById("q5-a3")
+var q5_answer_4 = document.getElementById("q5-a4")
+if (right_answer_2.checked == true) {
+    score++
+    alert('Q5 answer correct')
+}
+else{
+    alert('Q5 answer wrong')
+}
+
+
+
+var right_answer_2 = document.getElementById("q6-a2")
+var q6_answer_1 = document.getElementById("q6-a1")
+var q6_answer_3 = document.getElementById("q6-a3")
+var q6_answer_4 = document.getElementById("q6-a4")
+if (right_answer_2.checked == true) {
+    score++
+    alert('Q6 answer correct')
+}
+else{
+    alert('Q6 answer wrong')
+}
+
+
+var right_answer_1 = document.getElementById("q7-a1")
+var q7_answer_2 = document.getElementById("q7-a2")
+var q7_answer_3 = document.getElementById("q7-a3")
+var q7_answer_4 = document.getElementById("q7-a4")
+if (right_answer_1.checked == true) {
+    score++
+    alert('Q7 answer correct')
+}
+else{
+    alert('Q7 answer wrong')
+}
+
+
+
+var right_answer_2 = document.getElementById("q8-a2")
+var q8_answer_1= document.getElementById("q8-a1")
+var q8_answer_3 = document.getElementById("q8-a3")
+var q8_answer_4 = document.getElementById("q8-a4")
+if (right_answer_2.checked == true) {
+    score++
+    alert('Q8 answer correct')
+}
+else{
+    alert('Q8 answer wrong')
+}
+
+var right_answer_2 = document.getElementById("q9-a2")
+var q9_answer_1 = document.getElementById("q9-a1")
+var q9_answer_3 = document.getElementById("q9-a3")
+var q9_answer_4 = document.getElementById("q9-a4")
+if (right_answer_2.checked == true) {
+    score++
+    alert('Q9 answer correct')
+}
+else{
+    alert('Q9 answer wrong')
+}
+
+
+var right_answer_1 = document.getElementById("q10-a1")
+var q10_answer_2 = document.getElementById("q10-a2")
+var q10_answer_3 = document.getElementById("q10-a3")
+var q10_answer_4 = document.getElementById("q10-a4")
+if (right_answer_1.checked == true) {
+    score++
+    alert('Q10 answer correct')
+}
+else{
+    alert('Q10 answer wrong')
+}
+
+
+
+var right_answer_2 = document.getElementById("q11-a2")
+var q11_answer_1 = document.getElementById("q11-a1")
+var q11_answer_3 = document.getElementById("q11-a3")
+var q11_answer_4 = document.getElementById("q11-a4")
+if (right_answer_2.checked == true) {
+    score++
+    alert('Q11 answer correct')
+}
+else{
+    alert('Q11 answer wrong')
+}
+
+
+
+var right_answer_2 = document.getElementById("q12-a2")
+var q12_answer_2 = document.getElementById("q12-a1")
+var q12_answer_3 = document.getElementById("q12-a3")
+var q12_answer_4 = document.getElementById("q12-a4")
+if (right_answer_2.checked == true) {
+    score++
+    alert('Q12 answer correct')
+}
+else{
+    alert('Q12 answer wrong')
+}
+
+
+var right_answer_1 = document.getElementById("q13-a1")
+var q13_answer_2 = document.getElementById("q13-a2")
+var q13_answer_3 = document.getElementById("q13-a3")
+var q13_answer_4 = document.getElementById("q13-a4")
+if (right_answer_1.checked == true) {
+    score++
+    alert('Q13 answer correct')
+}
+else{
+    alert('Q13 answer wrong')
+}
+
+
+var right_answer_1 = document.getElementById("q14-a1")
+var q14_answer_2 = document.getElementById("q14-a2")
+var q14_answer_3 = document.getElementById("q14-a3")
+var q14_answer_4 = document.getElementById("q14-a4")
+if (right_answer_1.checked == true) {
+    score++
+    alert('Q14 answer correct')
+}
+else{
+    alert('Q14 answer wrong')
+}
+
+
+var right_answer_1 = document.getElementById("q15-a1")
+var q15_answer_2 = document.getElementById("q15-a2")
+var q15_answer_3 = document.getElementById("q15-a3")
+var q15_answer_4 = document.getElementById("q15-a4")
+if (right_answer_1.checked == true) {
+    score++
+    alert('Q15 answer correct')
+}
+else{
+    alert('Q15 answer wrong')
+}
+alert    
+("your score is " + score++)
+//   alert ('Conguralation Your Score is 100 percent')
+}
+
+// var right_answer_1 = document.getElementById("q7-a1")
+// var q7_answer_2 = document.getElementById("q7-a2")
+// var q7_answer_3 = document.getElementById("q7-a3")
+// var q7_answer_4 = document.getElementById("q7-a4")
+// if (right_answer_1.checked == true) {
+//     score++
+//     alert('Q7 answer correct')
+// }
+// else{
+//     alert('Q7 answer wrong')
+// }
+// alert    
+// ("your score is " + score++)
+
+// }
